35533e0568acf56999532c04a8866354afce4553
Mik1996 <michelemessina96@gmail.com>
2020-12-04 17:43:02 +0100

    Gruppo 3 Progetto COMPLETO 2

--- src/app/final-home/final-home/final-home.component.ts
+++ src/app/final-home/final-home/final-home.component.ts
@@ -13,42 +13,9 @@ export class FinalHomeComponent implements OnInit {
 
   message!: string;
   userAdmin!: boolean;
-  person = new Person();
-  error = false;
 
   constructor(private logRest: LogRestService,private router:Router) { }
 
   ngOnInit(): void {
-  }
-
-  doLogin(event:Event){
-    let response = this.logRest.login(this.person.username,this.person.password);
-    response.subscribe((answer)=> this.loginOk(answer), (error)=>this.loginNo(error));
-  }
-
-  private loginOk(answer:string):void{
-    this.error = false;
-    this.message = answer;
-    (window as any)["$"]('#staticBackdrop').modal('hide');
-    if(this.person.username === "user"){
-      this.router.navigateByUrl("/dashboard-user");
-    }else if(this.person.username === "admin"){
-      this.router.navigateByUrl("/dashboard-admin");
-    }
-  }
-
-  private loginNo(error: HttpErrorResponse): void{
-    this.error=true;
-    this.person.username = "";
-    this.person.password = "";
-    console.error(error);
-    setTimeout(() => {
-      this.error=false;
-    }, 5000);
-  }
-
-  annulla(event: Event): void{
-    this.person.username = "";
-    this.person.password = "";
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Person } from '../class/person';
 import { LogRestService } from '../service/log-rest.service';
 
 @Component({
@@ -10,7 +11,9 @@ import { LogRestService } from '../service/log-rest.service';
 })
 export class HeaderComponent implements OnInit {
 
+  person = new Person();
   message!: string;
+  error = false;
 
   constructor(public logRest: LogRestService, private router: Router) { }
 
@@ -20,6 +23,37 @@ export class HeaderComponent implements OnInit {
 
   checkRole(): string | undefined{
     return this.logRest.username;
+  }
+
+  doLogin(event:Event){
+    let response = this.logRest.login(this.person.username,this.person.password);
+    response.subscribe((answer)=> this.loginOk(answer), (error)=>this.loginNo(error));
+  }
+
+  private loginOk(answer:string):void{
+    this.error = false;
+    this.message = answer;
+    (window as any)["$"]('#staticBackdrop').modal('hide');
+    console.log("ciao");
+    if(this.person.username === "user"){
+      this.router.navigateByUrl("/dashboard-user");
+      console.log("ciao");
+    }else if(this.person.username === "admin"){
+      console.log("ciao");
+      this.router.navigateByUrl("/dashboard-admin");
+    }
+    this.person.username = "";
+    this.person.password = "";
+  }
+
+  private loginNo(error: HttpErrorResponse): void{
+    this.error=true;
+    this.person.username = "";
+    this.person.password = "";
+    console.error(error);
+    setTimeout(() => {
+      this.error=false;
+    }, 5000);
   }
 
   onLogout(event:Event):void{
@@ -34,6 +68,11 @@ export class HeaderComponent implements OnInit {
 
   private onLogoutKo(error: HttpErrorResponse): void{
     console.error(error);
+  }
+
+  annulla(event: Event): void{
+    this.person.username = "";
+    this.person.password = "";
   }
 
 }
